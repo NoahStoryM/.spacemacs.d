f506f8b1d89669d9782cd36b22ac0c71cf4bcc45
NoahStoryM <noahstorym@gmail.com>
2021-10-17 15:52:29 +0800

    evil-ex-lazy-highlight inherit lazy-highlight.

--- packages/spacemacs-theme/spacemacs-common.el
+++ packages/spacemacs-theme/spacemacs-common.el
@@ -406,7 +406,7 @@ to 'auto, tags may not be properly aligned. "
      `(ess-r-signal-keyword-face ((,class (:foreground ,war))))
 
 ;;;;; evil
-     `(evil-ex-lazy-highlight ((,class (:background ,mat :foreground ,bg1))))
+     `(evil-ex-lazy-highlight ((,class (:inherit lazy-highlight))))
      `(evil-ex-substitute-matches ((,class (:background ,red-bg :foreground ,red))))
      `(evil-ex-substitute-replacement ((,class (:background ,green-bg :foreground ,green))))
 

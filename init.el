2fd7d0d7d900006f904e7046f72c34a3d615a3f7
NoahStoryM <noahstorym@gmail.com>
2022-08-06 07:43:51 +0800

    Use `DejaVu Sans Mono` instead of `Ubuntu Mono`.

--- init.el
+++ init.el
@@ -342,8 +342,8 @@ It should only modify the values of Spacemacs settings."
    ;; Default font or prioritized list of fonts. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("Ubuntu Mono"
-                               :size 26
+   dotspacemacs-default-font '("DejaVu Sans Mono"
+                               :size 24
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
